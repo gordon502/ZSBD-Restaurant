641ecbeafb56b95bb7534a5bd1929299c6d622af
tok <kozjach@gmail.com>
2020-01-07 19:05:02 +0100

    Fix connecting to the base.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.sql.*;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -17,21 +18,30 @@ import java.util.logging.Logger;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
         ConnectionData.conn = null;
         Properties connectionProps = new Properties();
         connectionProps.put("user", "sys as sysdba");
         connectionProps.put("password", "Oracle123");
         try {
-            ConnectionData.conn = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1524/"+
-            "XE", connectionProps);
+            ConnectionData.conn = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1524/" +
+                    "XE", connectionProps);
             System.out.println("Połączono z bazą danych");
         } catch (SQLException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,
-                    "nie udało się połączyć z bazą danych", ex);
-            System.exit(-1);
+            try {
+                connectionProps.put("user", "ot");
+                connectionProps.put("password", "Orcl1234");
+                ConnectionData.conn = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/"+"pdborcl", connectionProps);
+                System.out.println("Połączono z bazą danych");
+
+            } catch (SQLException ex2) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE,
+                        "nie udało się połączyć z bazą danych", ex2);
+                System.exit(-1);
+            }
         }
+
 
         UserList.readUsers();
         StockItemList.readItems();
