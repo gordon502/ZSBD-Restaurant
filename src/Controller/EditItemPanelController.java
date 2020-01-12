66a06c03a165956b4b5aaa67b403b5064e55a882
gordon502 <gordonarek@gmail.com>
2020-01-12 16:03:53 +0100

    Add EditItemPanelScene with controller skeleton.

---
+++ src/Controller/EditItemPanelController.java
@@ -0,0 +1,78 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class EditItemPanelController {
+
+    @FXML
+    private TableView<?> supplierTable1;
+
+    @FXML
+    private TableView<?> supplierTable12;
+
+    @FXML
+    private GridPane dataGrid;
+
+    @FXML
+    private TextField phoneTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField vatTextField;
+
+    @FXML
+    private ComboBox<?> foodCategoryComboBox;
+
+    @FXML
+    private Label userLabel;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button modifyButton;
+
+    @FXML
+    private Button activeButton;
+
+    @FXML
+    private Button foodCategoriesButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void activeButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modifyItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showFoodCategoriesPanel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showMainMenu(ActionEvent event) {
+
+    }
+
+}
